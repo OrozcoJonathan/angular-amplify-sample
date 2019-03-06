367e97266c0374ef6f71a91507bead27850c9ef2
Jonathan Orozco <jonatho1@uci.edu>
2019-03-05 16:16:24 -0800

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material';
+import { StorageComponent } from './storage/storage.component';
+import { InteractionsComponent } from './interactions/interactions.component';
+import { HomeComponent } from './home/home.component';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    StorageComponent,
+    InteractionsComponent,
+    HomeComponent,
   ],
   imports: [
+    AmplifyAngularModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+
   ],
-  providers: [],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
